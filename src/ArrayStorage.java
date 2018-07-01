efa71c50327d579b6541bd851de7dadc4adf6f5b
Ivan Abramov <madrid-beckham@yandex.ru>
2018-07-02 09:11:42 +1000

    HW1_1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+
+import static java.util.Arrays.copyOf;
+import static java.util.Arrays.stream;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +10,60 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++)
+            storage[i] = r;
+
     }
 
     Resume get(String uuid) {
-        return null;
+
+        for (int i = 0; i < storage.length; i++) {
+
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+
+        }
+        return null; // т.е. значит резюме нет
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+
+            } else {
+                storage[i].uuid = null;
+            }
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+
+        return copyOf(storage, storage.length);
+
+
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].equals(null)) {
+                System.out.println("null");
+
+            } else {
+                count++;
+            }
+
+        }
+        return count;
+
     }
 }
